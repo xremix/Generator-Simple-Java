9d3e7bb3bb9e601431c54675e0031e6a06fdc363
Toni Hoffmann <toni-hoffmann@xremix.de>
2017-12-12 19:11:52 +0100

    make the class names work better and have option for include exercise

--- app/index.js
+++ app/index.js
@@ -15,17 +15,29 @@ module.exports = class extends Generator{
 			name    : 'className',
 			message : 'Classname',
 			default : 'MyClass'
+		},{
+			type	: 'checkbox',
+			name	: 'excercise',
+			message : 'Do you want to add a exercise class?',
+			store	: true,
+			choices:[{
+				name: 'Include exercise',
+				value: 'includeExercise',
+				checked: true
+			}]
 		}];
 		return this.prompt(prompts).then((answer)=>{
-			this.className = answer.className+'Excercise.java';
+			this.className = answer.className;
+			this.includeExercise = answer.excercise && answer.excercise.indexOf('includeExercise') !== -1;
 			done();
 		});
 	}
 	writing () {
 		this.fs.copyTpl(
 			this.templatePath('_templ.java'),
-			this.destinationPath(this.className),{
-				className: this.className
+			this.destinationPath(this.className+ (this.includeExercise ? 'Excercise.java' : '.java')),{
+				className: this.className,
+				includeExercise: this.includeExercise
 			}
 		);
 	}
--- app/templates/_templ.java
+++ app/templates/_templ.java
@@ -6,10 +6,16 @@ class <%= className %>{
 	public <%= className %>(){
 		System.out.println("<%= className %>");
 	}
+	<% if(!includeExercise) { %>
+	public static void main(String[] args){
+		<%= className %> a = new <%= className %>();
+	}
+	<% } %>
 }
-
+<% if(includeExercise) { %>
 public class <%= className %>Excercise{
 	public static void main(String[] args){
 		<%= className %> a = new <%= className %>();
 	}
 }
+<% } %>
