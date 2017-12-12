e6cafce0067048abd93e191c831ae03c65072f2e
Toni Hoffmann <toni-hoffmann@xremix.de>
2017-12-12 18:30:25 +0100

    first version of template

---
+++ app/index.js
@@ -0,0 +1,30 @@
+'use strict';
+var Generator = require('yeoman-generator');
+
+module.exports = class extends Generator{
+	constructor(args, opts) {
+		//Calling super constructor
+		super(args, opts);
+	}
+	promting(){
+		var done = this.async();
+		this.projectVersion = "0.0.1";
+
+		var prompts = [{
+			type    : 'input',
+			name    : 'sourceFilename',
+			message : 'Classname',
+			default : 'MyClass'
+		}];
+		return this.prompt(prompts).then((answer)=>{
+			this.sourceFilename = answer.sourceFilename+'.java';
+			done();
+		});
+	}
+	writing () {
+		this.fs.copyTpl(
+			this.templatePath('_templ.java'),
+			this.destinationPath(this.sourceFilename)
+		);
+	}
+};
---
+++ app/templates/_templ.java
@@ -0,0 +1,14 @@
+// import java.util.ArrayList;
+// import java.util.Scanner;
+
+class ExceptionHandling{
+	public ExceptionHandling(){
+		System.out.println("ExceptionHandling");
+	}
+}
+
+public class ExceptionHandlingAufgabe{
+	public static void main(String[] args){
+		ExceptionHandling a = new ExceptionHandling();
+	}
+}
